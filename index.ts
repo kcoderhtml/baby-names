9e2e742beaa0277cbbc02849115f8afa5e748c3a
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-10 12:10:02 -0400

    feat: add the basic check for data

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+console.log("#---------------------------->\n  ","Welcome to the 👶 parser!", "\n#---------------------------->\n")
+
+// check whether the file names.zip exists in the data directory
+const exists = await Bun.file("data/names.zip").exists()
+
+if (!exists) {
+    console.log("I'm sorry but you don't seem to have downloaded the names file")
+    console.log("Please download it from:", "https://www.ssa.gov/oact/babynames/names.zip")
+    // exit
+    process.exit(1)
+}
+
